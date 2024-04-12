1c2061881bbf207e241a740bf739253dfd9bf0a9
Sandeep Chauhan <sandeepchauhan2709@gmail.com>
2024-04-13 02:26:39 +0530

    Initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({task,deleteTodo}) => {
+  return (
+    <div className='Todo'>
+    
+      <p>{task.task}</p>
+
+      <div className='icon'>
+           <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { TodoForm } from './TodoForm'
+import { v4 as uuidv4} from 'uuid';
+import { Todo } from './Todo';
+
+uuidv4();
+
+export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+          setTodos([...todos,{id:uuidv4(),task: todo,
+          completed: false, isEditing:false}])
+          console.log(todos)
+    }
+ 
+    const deleteTodo = id => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+ 
+  return (
+    <div className='TodoWrapper'>
+
+        <h1>TODO Task List</h1>
+
+        <TodoForm addTodo={addTodo}/>
+
+       {todos.map((todo,index) => (
+        <Todo task={todo} key={index}
+        deleteTodo = {deleteTodo}
+        />
+       ))} 
+
+    </div>
+  )
+}
